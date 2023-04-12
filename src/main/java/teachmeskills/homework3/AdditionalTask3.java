6fc7e3378e321dcf5cb21bd943eaeebfc6bb13b9
asidoryk <asidoryk@eisgroup.com>
2023-04-12 20:51:45 +0200

    commit updated tasks from homework 3

--- src/main/java/teachmeskills/homework3/AdditionalTask3.java
+++ src/main/java/teachmeskills/homework3/AdditionalTask3.java
@@ -2,34 +2,10 @@ package teachmeskills.homework3;
 
 public class AdditionalTask3 {
     public static void main(String[] args) {
-        calc(2, 5, '*');
-        calc(90, 55, '-');
-        calc(456, 2, '+');
-        calc(100, 4, '/');
-    }
-
-    public static void calc(int num1, int num2, char operation) {
-        int result;
-        switch (operation) {
-            case '+':
-                result = num1 + num2;
-                System.out.println(result);
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.println(result);
-
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.println(result);
-                break;
-            case '/':
-                result = num1 / num2;
-                System.out.println(result);
-                break;
-            default:
-                System.out.println("Operation is not supported, please try again");
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " * " + j + " = " + i * j);
+            }
         }
     }
 }
